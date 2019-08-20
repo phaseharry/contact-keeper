da753f69f07a4176b838e7f287d363c087c8ebb3
Harry Chen <harrychenhc@icloud.com>
2019-08-20 14:44:15 -0400

    hook up MongoDB Atlas to the server

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoURI')
+/*
+  config allows us to store variables inside the /config/default.json file and have access it within our entire application.
+  In line 3, we're using config to return the mongoURI key we stored inside the default.json file
+*/
+
+const connectDB = () => {
+  mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => {
+      console.error(err.message)
+      process.exit(1)
+    })
+}
+
+module.exports = connectDB
